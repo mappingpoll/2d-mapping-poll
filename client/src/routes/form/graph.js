c9a6531760b4c20301be40516adff72d63135cc2
Nicolas Epstein <nilueps@gmail.com>
2021-01-29 16:51:20 -0500

    graph svg and cross-browser tinkering

--- client/src/routes/form/graph.js
+++ client/src/routes/form/graph.js
@@ -1,24 +1,37 @@
 import { h } from "preact";
-import { useLayoutEffect, useState } from "preact/hooks";
+import { useLayoutEffect, useRef, useState } from "preact/hooks";
 import { Text } from "preact-i18n";
 import style from "./graph.css";
+
+const DotSVG = (props) => {
+  const { top, left, size } = props;
+  const COLOR = "red";
+  return <circle cx={left} cy={top} r={size} fill={COLOR} />;
+};
 
 const Graph = (props) => {
   let initX, initY;
 
-  const DOT_BASE_SIZE = 100;
+  const DOT_MAX_SIZE = 220;
+  const DOT_MIN_SIZE = 2;
+  const BLUR_RADIUS = 100;
   // the data we want
-  let [dotXY, setDotXY] = useState([0, 0]);
-  let [confidence, setConfidence] = useState(100);
+  let [dots, setDots] = useState([]);
+  let [dotXY, setDotXY] = useState(["0", "0"]);
+  let [confidence, setConfidence] = useState("100");
   // ui data
-  let [dotSize, setDotSize] = useState(2);
+  let [dotSize, setDotSize] = useState(DOT_MIN_SIZE.toString());
   let [dotIsVisible, setDotIsVisible] = useState(false);
+  let [blur, setBlur] = useState("none");
+
+  // dom refs
+  const g = useRef(null);
 
   function exposeValues() {
-    props.returnValues({x: dotXY[0], y: dotXY[1], confidence})
+    props.returnValues({ x: dotXY[0], y: dotXY[1], confidence });
   }
 
-  useLayoutEffect(exposeValues, [dotXY, confidence])  
+  useLayoutEffect(exposeValues, [dotXY, confidence]);
 
   // Drag functionnality
   function handlePointerDown(event) {
@@ -26,7 +39,7 @@ const Graph = (props) => {
     const rect = graph.getBoundingClientRect();
     initX = rect.left;
     initY = rect.top;
-    repositionDot(event)
+    repositionDot(event);
     graph.addEventListener("pointermove", repositionDot, false);
     function removeListener() {
       graph.removeEventListener("pointermove", repositionDot, false);
@@ -40,39 +53,48 @@ const Graph = (props) => {
     if (!dotIsVisible) setDotIsVisible(true);
     let x = event.clientX - initX;
     let y = event.clientY - initY;
-    x = singleDecimal(x);
-    y = singleDecimal(y);
+    x = rounded(x);
+    y = rounded(y);
     setDotXY([x, y]);
   }
-
+  function calcBlur() {
+    const radius = (1 - confidence / 100) * BLUR_RADIUS;
+    return radius.toString(); //`blur(${radius}px)`;
+  }
   function resizeDot(event) {
-    setConfidence(event.target.value)
-    setDotSize(singleDecimal(DOT_BASE_SIZE * (102 - confidence) / 100));
+    setConfidence(event.target.value);
+    setBlur(calcBlur());
+    setDotSize(rounded((DOT_MAX_SIZE * (102 - confidence)) / 100));
   }
 
-  function singleDecimal(x) {
-    return Number.parseFloat(x).toFixed(1);
+  function rounded(x) {
+    return Number.parseInt(x).toString();
   }
 
   return (
     <div class={style.graphContainer}>
-      <p class={style.labelTop}>{props.labelTop}</p>
-      <div class={style.graphBox}>
+      <svg>
+        <defs>
+          <filter id="blurMe">
+            <feGaussianBlur stdDeviation={blur} />
+          </filter>
+        </defs>
+      </svg>
+      <div class={style.labelTop}>{props.labelTop}</div>
+      <div class={style["vertical-center"]}>
         <div class={style.labelLeft}>{props.labelLeft}</div>
-        <div class={style.graph} onPointerDown={handlePointerDown}>
-          {dotIsVisible && <div
-            class={style.dot}
-            style={{
-              top: dotXY[1] - dotSize + "px",
-              left: dotXY[0] - dotSize + "px",
-              filter: `blur(calc(${1 - confidence / 100} * var(--fuzz-radius))`,
-              borderWidth: dotSize + "px",
-            }}
-          />}
+        <div ref={g} class={style.graph} onPointerDown={handlePointerDown}>
+          <svg
+            class={style["graph-svg"]}
+            
+          >
+            ({dotIsVisible} &&{" "}
+            <DotSVG top={dotXY[1]} left={dotXY[0]} size={dotSize} />)
+          </svg>
         </div>
-        <p class={style.labelRight}>{props.labelRight}</p>
+        <div class={style.labelRight}>{props.labelRight}</div>
       </div>
-      <p class={style.labelBottom}>{props.labelBottom}</p>
+      <div class={style.labelBottom}>{props.labelBottom}</div>
       <p>
         <Text id="graph.position" fields={{ x: dotXY[0], y: dotXY[1] }}>
           Position: x = {dotXY[0]}, y = {dotXY[1]}
