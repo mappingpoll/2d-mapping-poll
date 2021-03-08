25f9ae270737606a2eab48a5a6768dcff8a766e5
Nicolas Epstein <nilueps@gmail.com>
2021-03-08 13:13:57 -0500

    added unique id to each respondant

--- client/src/routes/results/fetch/parseLocalCSV.js
+++ client/src/routes/results/fetch/parseLocalCSV.js
@@ -1,21 +1,26 @@
 import { csv } from "d3-fetch";
 import { NA_SYMBOL, UNCERTAINTY } from "../constants";
-import jitter from "./jitter"
+import jitter from "./jitter";
 
 // load & parse the result data asynchronously
 export async function parseLocalCSV(path) {
-  return await csv(path, (d) => {
-      // convert strings to numbers where appropriate
-      const row = d;
-      for (let col in row) {
-        if (col !== "poll" && col != "Language" && row[col] !== NA_SYMBOL) {
-          row[col] = +row[col] + jitter(UNCERTAINTY);
-        }
+  return await csv(path, d => {
+    // convert strings to numbers where appropriate
+    const row = d;
+    for (let col in row) {
+      if (col !== "poll" && col != "Language" && row[col] !== NA_SYMBOL) {
+        row[col] = +row[col] + jitter(UNCERTAINTY);
       }
-      return row;
+    }
+    return row;
+  })
+    .then(data => {
+      // give unique ids to each respondant
+      data.forEach((d, i) => (d.id = i));
+      return data;
     })
-    .then((data) => {
+    .then(data => {
       console.log("csv loaded");
-      return data
+      return data;
     });
 }
